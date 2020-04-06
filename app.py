f6cdaa4d832aa8f6b414c33a9b4d0d8a97cfae2f
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-04-06 07:16:53 +0000

    Added further structure to project workspace, including installing the appropriate mongodb, python and flask packages, adding base.html, initial code for connecting flask and mongoDB to application, and adding requirements.txt and Procfile files to ensure application can be pushed to heroku

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+
+
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_DBNAME"] = 'task_manager'
+
+mongo = PyMongo(app)
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ['MONGO_URI'] = 'mongodb+srv://Seved:Stockholm12@gustafsfirstcluster-jtrsh.mongodb.net/task_manager?retryWrites=true&w=majority'
