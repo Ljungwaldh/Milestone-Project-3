71c219d8e364d4d67300e46dfef44174a7ae5fb2
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-04-06 14:23:40 +0000

    tested connection to app.py with a simple Hello World function, this test working

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ app.config["MONGO_DBNAME"] = 'task_manager'
 mongo = PyMongo(app)
 
 
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
